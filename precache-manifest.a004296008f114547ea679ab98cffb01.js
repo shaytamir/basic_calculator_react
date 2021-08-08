d9d951bc9f8724ccc1138f162a61311aee3972b9
shay <shay.lom.tamir@gmail.com>
2021-08-08 18:29:11 +0300

    Updates

---
+++ precache-manifest.a004296008f114547ea679ab98cffb01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f9bec4edb572c100da496add6ed8a7",
+    "url": "//shaytamir.github.io/calculator_react/index.html"
+  },
+  {
+    "revision": "6dbfcd1c1bf15ae59da2",
+    "url": "//shaytamir.github.io/calculator_react/static/css/main.9385092b.chunk.css"
+  },
+  {
+    "revision": "91f73be4f7306fdfc326",
+    "url": "//shaytamir.github.io/calculator_react/static/js/2.9f844e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//shaytamir.github.io/calculator_react/static/js/2.9f844e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbfcd1c1bf15ae59da2",
+    "url": "//shaytamir.github.io/calculator_react/static/js/main.e3da05ac.chunk.js"
+  },
+  {
+    "revision": "dd2bb6e461d7267a0669",
+    "url": "//shaytamir.github.io/calculator_react/static/js/runtime-main.9de53abe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//shaytamir.github.io/calculator_react/precache-manifest.3eeb7dedf5cbcfdbf075a5502f263a65.js"
+  "//shaytamir.github.io/calculator_react/precache-manifest.a004296008f114547ea679ab98cffb01.js"
 );
 
 self.addEventListener('message', (event) => {
